d5ab231bc65e9b9b326b963ea9110ff6a4734c0c
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-04-24 20:45:24 +0100

    feat: basic setup

--- use-format-amount-input/src/useFormatAmountInput.js
+++ use-format-amount-input/src/useFormatAmountInput.js
@@ -1,6 +1,24 @@
+import {useState} from "react"
+
 export default function useFormatAmountInput (settings = {}) {
-  const { message } = settings;
+  const [amount, setAmount] = useState("")
+
+
+  const handleAmountChange = (e) =>{
+    const keyPressed = e.nativeEvent.data
+		const keyPattern = /\d/
+
+		if (keyPattern.test(Number(keyPressed))) {
+			const amountValue = e.target.value.trim()
+			const strippedAmount = amountValue.replaceAll(",", "")
+			const amountString = strippedAmount.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+			setAmount(amountString)
+		}
+  }
+
+
   return {
-    message
+    amount,
+    handleAmountChange
   };
 }
