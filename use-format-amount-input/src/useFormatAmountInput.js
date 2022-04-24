cb89b433120da967732d60e52d69aebaf9cf1584
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-04-24 21:35:33 +0100

    feat: add support for decimal places

--- use-format-amount-input/src/useFormatAmountInput.js
+++ use-format-amount-input/src/useFormatAmountInput.js
@@ -8,12 +8,25 @@ export default function useFormatAmountInput (settings = {}) {
     const keyPressed = e.nativeEvent.data
 		const keyPattern = /\d/
 
-		if (keyPattern.test(Number(keyPressed))) {
+		if (keyPattern.test(Number(keyPressed)) || (keyPressed.includes(".") && !amount.includes("."))) {
 			const amountValue = e.target.value.trim()
 			const strippedAmount = amountValue.replaceAll(",", "")
-			const amountString = strippedAmount.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-			setAmount(amountString)
-		}
+
+      const amountValueArray = strippedAmount.split(".")
+
+      const leftHandside = amountValueArray[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      const rightHandside = amountValueArray[1]
+
+      let value = leftHandside
+
+      if(amountValueArray.length === 2){
+        value += `.${rightHandside}`
+      }
+		
+			setAmount(value)
+		}else{
+      return
+    }
   }
 
 
