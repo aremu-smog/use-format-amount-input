161d4c56af625777afcf170eb7040a93409a0dc8
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-04-24 20:29:24 +0100

    [use-custom-hook] Initialized Project

---
+++ use-format-amount-input/src/index.js
@@ -0,0 +1,1 @@
+export { default as useFormatAmountInput } from './useFormatAmountInput';
---
+++ use-format-amount-input/src/useFormatAmountInput.js
@@ -0,0 +1,6 @@
+export default function useFormatAmountInput (settings = {}) {
+  const { message } = settings;
+  return {
+    message
+  };
+}
