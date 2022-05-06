2050b1137577064aa440e04c6878d3dbcc91b597
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-05-06 07:51:24 +0100

    refactor: create support for props and make it backward compatible

--- use-format-amount-input/src/useFormatAmountInput.js
+++ use-format-amount-input/src/useFormatAmountInput.js
@@ -1,62 +1,77 @@
-import {useState} from "react"
+import { useState } from "react"
 
-export default function useFormatAmountInput (decimalPlaces = 0) {
-  const [amount, setAmount] = useState("")
+export default function useFormatAmountInput(props) {
+	// This check was made to ensure backwards compatibility with accpeting only the decimal place as a parameter.
+	let decimalPlaces = typeof props === "number" ? props : props?.decimalPlaces
 
-  const handleAmountChange = (e) =>{
-    const keyPressed = e.nativeEvent.data
+	// Destructure other properties here
 
-    
-    const amountValue = e.target.value.trim()
+	const [amount, setAmount] = useState("")
 
-    
-    const cursorPosition = e.target.selectionStart
+	const handleAmountChange = e => {
+		const keyPressed = e.nativeEvent.data
+
+		const amountValue = e.target.value.trim()
+
+		const cursorPosition = e.target.selectionStart
 		const keyPattern = /\d/
 
-		if (keyPattern.test(Number(keyPressed)) || (keyPressed.includes(".") && !amount.includes("."))) {
+		if (
+			keyPattern.test(Number(keyPressed)) ||
+			(keyPressed.includes(".") && !amount.includes("."))
+		) {
 			const strippedAmount = amountValue.replaceAll(",", "")
 
-      const amountValueArray = strippedAmount.split(".")
+			const amountValueArray = strippedAmount.split(".")
 
-      const leftHandside = amountValueArray[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-      const rightHandside = amountValueArray[1]
+			const leftHandside = amountValueArray[0].replace(
+				/\B(?=(\d{3})+(?!\d))/g,
+				","
+			)
+			const rightHandside = amountValueArray[1]
 
-      if(decimalPlaces && rightHandside?.length > decimalPlaces){
-        return
-      }
+			if (decimalPlaces && rightHandside?.length > decimalPlaces) {
+				return
+			}
 
-      let value = leftHandside
+			let value = leftHandside
 
-      if(amountValueArray.length === 2){
-        value += `.${rightHandside}`
-      }
-		
+			if (amountValueArray.length === 2) {
+				value += `.${rightHandside}`
+			}
+
 			setAmount(value)
-		}else{
-      return
-    }
+		} else {
+			return
+		}
 
-    setTimeout(()=>{
-      const targetValue = e.target.value
-      const hasCommas = (/,/g).test(targetValue)
-      const inputType = e.nativeEvent.inputType
+		setTimeout(() => {
+			const targetValue = e.target.value
+			const hasCommas = /,/g.test(targetValue)
+			const inputType = e.nativeEvent.inputType
 
-      const isDeleting = inputType === "deleteContentBackward" || inputType === "deleteContentForward" || inputType === "deleteContent" || inputType === "deleteByCut"
+			const isDeleting =
+				inputType === "deleteContentBackward" ||
+				inputType === "deleteContentForward" ||
+				inputType === "deleteContent" ||
+				inputType === "deleteByCut"
 
- 
-      if(hasCommas && ((cursorPosition + 1)  === targetValue.length) && !isDeleting){   
-        e.target.selectionStart = cursorPosition  + 1
-        e.target.selectionEnd = cursorPosition  + 1
-      }else{
-        e.target.selectionStart = cursorPosition  
-        e.target.selectionEnd = cursorPosition  
-      }
-    },0)
-  }
+			if (
+				hasCommas &&
+				cursorPosition + 1 === targetValue.length &&
+				!isDeleting
+			) {
+				e.target.selectionStart = cursorPosition + 1
+				e.target.selectionEnd = cursorPosition + 1
+			} else {
+				e.target.selectionStart = cursorPosition
+				e.target.selectionEnd = cursorPosition
+			}
+		}, 0)
+	}
 
-
-  return {
-    amount,
-    handleAmountChange
-  };
+	return {
+		amount,
+		handleAmountChange,
+	}
 }
